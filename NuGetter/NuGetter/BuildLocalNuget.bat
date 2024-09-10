4dfff00d95edaf7bfded402665f99571bd19ba22
Samu Ruuhonen <samu.ruuhonen@trimble.com>
2024-09-10 16:54:41 +0300

    Updated nuget creation for versions 1.2.59.x

--- NuGetter/NuGetter/BuildLocalNuget.bat
+++ NuGetter/NuGetter/BuildLocalNuget.bat
@@ -1,8 +1,8 @@
 @echo off
 
 set Major=1
-set Minor=6
-set Patch=37
+set Minor=2
+set Patch=59
 set /p Build=<build.txt
 set /a Build+=1
 echo %Build% >build.txt
@@ -14,6 +14,6 @@ MSBuild.exe .. /p:Configuration=142-DebugCRT /p:Platform=x64 /p:MajorVersion=%Ma
 MSBuild.exe .. /p:Configuration=140-Release /p:Platform=x64 /p:MajorVersion=%Major% /p:MinorVersion=%Minor% /p:PatchVersion=%Patch% /p:BuildVersion=%Build% /maxCpuCount
 MSBuild.exe .. /p:Configuration=142-Release /p:Platform=x64 /p:MajorVersion=%Major% /p:MinorVersion=%Minor% /p:PatchVersion=%Patch% /p:BuildVersion=%Build% /maxCpuCount
 
-c:\work\nuget.exe pack -Version %Major%.%Minor%.%Patch%.%Build% LibPng-Tekla.nuspec
-c:\work\nuget.exe pack -Version %Major%.%Minor%.%Patch%.%Build% LibPng-Tekla.redist.nuspec
+rem c:\work\nuget.exe pack -Version %Major%.%Minor%.%Patch%.%Build% LibPng-Tekla.nuspec
+rem c:\work\nuget.exe pack -Version %Major%.%Minor%.%Patch%.%Build% LibPng-Tekla.redist.nuspec
 c:\work\nuget.exe pack -Version %Major%.%Minor%.%Patch%.%Build% LibPng-Tekla.Static.nuspec
